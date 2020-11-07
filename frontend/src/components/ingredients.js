00137f411c6419c72da3ee7cd0e15c7cfe30612a
mavrik83 <rjhuber83@gmail.com>
2020-11-07 00:50:13 -0500

    add ingredients constructor

--- frontend/src/components/ingredients.js
+++ frontend/src/components/ingredients.js
@@ -1,1 +1,7 @@
 class Ingredients {
+  constructor() {
+    this.adapter = new IngredientsAdapter();
+    this.ingredientDropDown = document.getElementById("filter-dropdown");
+    this.fetchAndPopulateDropDown();
+  }
+
