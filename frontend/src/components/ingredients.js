4089dde1ae15cc0f7f0d2c4f3b3c3171e59b2ea7
mavrik83 <rjhuber83@gmail.com>
2020-11-07 00:51:50 -0500

    add method to populate ingredient list

--- frontend/src/components/ingredients.js
+++ frontend/src/components/ingredients.js
@@ -9,3 +9,13 @@ class Ingredients {
     this.adapter.getIngredients().then(json => this.populateIngredientDropDown(json))
   }
 
+  populateIngredientDropDown(data) {
+    data.sort((a, b) => (a.attributes.name > b.attributes.name) ? 1 : -1)
+    for (let ingredient of data) {
+      let option = document.createElement("option")
+      option.value = ingredient.attributes.name
+      option.innerHTML = ingredient.attributes.name
+      this.ingredientDropDown.appendChild(option)
+    }
+  }
+}
