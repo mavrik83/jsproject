6f8870b37fd2314356ac88baf8355c3ba5364739
mavrik83 <rjhuber83@gmail.com>
2020-11-07 00:52:56 -0500

    add recipe card builder function

--- frontend/src/components/recipe.js
+++ frontend/src/components/recipe.js
@@ -6,3 +6,36 @@ class Recipe {
       this.ingredients = ingredients;
   }
 
+  createRecipeCard() {
+    const card = document.createElement('div')
+    card.className = "card"
+    const img = document.createElement('img')
+    img.src = this.imageLink
+    card.appendChild(img)
+    const cardInfo = document.createElement('div')
+    cardInfo.className = "card-info"
+    const title = document.createElement('h1')
+    title.innerHTML = this.title
+    cardInfo.appendChild(title)
+    const ingHeader = document.createElement('h3')
+    ingHeader.innerHTML = "Ingredients:"
+    cardInfo.appendChild(ingHeader)
+    const ul = document.createElement('ul')
+    for (let ingredient of this.ingredients) {
+      let li = document.createElement('li')
+      li.innerHTML = ingredient
+      ul.appendChild(li)
+    }
+    cardInfo.appendChild(ul)
+    const footer = document.createElement('div')
+    footer.className = "card-footer"
+    const link = document.createElement('a')
+    link.target = "_blank"
+    link.href = this.recipeLink
+    link.innerHTML = "View Recipe Here"
+    footer.appendChild(link)
+    card.appendChild(cardInfo)
+    card.appendChild(footer)
+    document.getElementById('recipe-card-container').appendChild(card)
+  }
+}
