c5955aad6fb5e60c9e1e0684a435b16c7fdf881a
mavrik83 <rjhuber83@gmail.com>
2020-11-07 00:19:29 -0500

    Create recipes_controller.rb

---
+++ backend/recipe-api/app/controllers/recipes_controller.rb
@@ -0,0 +1,33 @@
+class RecipesController < ApplicationController
+  def index
+    recipes = Recipe.all
+
+    render json: RecipeSerializer.new(recipes)
+  end
+
+  def show
+    recipes = Ingredient.find_by(name: params[:ingredient_name]).recipes
+
+    render json: RecipeSerializer.new(recipes.sample)
+  end
+
+  def create
+    recipe = Recipe.new(recipe_params)
+    ingredients = params[:ingredients].map { |ingredient| Ingredient.find_or_create_by(name: ingredient) }
+    recipe.ingredients << ingredients
+    recipe.save
+    render json: RecipeSerializer.new(recipe)
+  end
+
+  def delete
+    recipe = Recipe.find_by(params[:id])
+    recipe.destroy
+    show
+  end
+
+  private
+
+  def recipe_params
+    params.require(:recipe).permit(:title, :recipe_link, :image_link)
+  end
+end
