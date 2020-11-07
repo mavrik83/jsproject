41188218d5e930b07755fba7a22eb60ee782cfc9
mavrik83 <rjhuber83@gmail.com>
2020-11-07 00:53:39 -0500

    add function to load recipes from api

--- frontend/src/components/recipes.js
+++ frontend/src/components/recipes.js
@@ -12,3 +12,25 @@ class Recipes {
     this.fetchAndLoadRecipes();
   }
 
+  fetchAndLoadRecipes() {
+    this.adapter.getRecipes().then(recipes => this.createRecipes(recipes)).then(() => this.addRecipesToDom())
+  }
+
+  bindEventListeners() {
+    this.formSubmit.addEventListener("click", function() {
+      event.preventDefault();
+      this.addRecipe();
+    }.bind(this))
+    this.addRecipeButton.addEventListener("click", function() {
+      this.toggleForm();
+      this.toggleButtons();
+    }.bind(this))
+    this.dropDownButton.addEventListener("click", function() {
+      this.toggleDropDown();
+      this.toggleButtons();
+    }.bind(this))
+    this.ingredientDropDown.addEventListener("change", function() {
+      this.getAndLoadRandomRecipeByIngredient();
+    }.bind(this))
+  }
+
