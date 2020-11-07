129fb69b3e201fbe4e4867ce39b79105a24b0076
mavrik83 <rjhuber83@gmail.com>
2020-11-07 00:20:13 -0500

    add rest routes for api

---
+++ backend/recipe-api/config/routes.rb
@@ -0,0 +1,8 @@
+Rails.application.routes.draw do
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get '/recipes' => 'recipes#index'
+  get '/recipes/:ingredient_name' => 'recipes#show'
+  post '/recipes' => 'recipes#create'
+
+  get '/ingredients' => 'ingredients#index'
+end
