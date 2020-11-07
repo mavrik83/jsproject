40035f2270290872b1c0a29c7f47e23e7363f20e
mavrik83 <rjhuber83@gmail.com>
2020-11-07 00:52:40 -0500

    add recipe constructor

--- frontend/src/components/recipe.js
+++ frontend/src/components/recipe.js
@@ -1,1 +1,8 @@
 class Recipe {
+  constructor(title, imageLink, recipeLink, ingredients) {
+      this.title = title;
+      this.imageLink = imageLink;
+      this.recipeLink = recipeLink;
+      this.ingredients = ingredients;
+  }
+
